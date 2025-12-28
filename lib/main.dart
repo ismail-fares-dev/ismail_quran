fe991daaf0a14ed4ad28f645dd29ae7dac1198ec
Ismail Fares <i.fares90@outlook.de>
2025-12-28 11:23:03 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/homescreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute:
+          HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName:
+            (context) => HomeScreen(),
+      },
+    );
+  }
+}
